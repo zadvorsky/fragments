61c000bd25d39bd96c2561b1ec28b5c8e993662f
Szenia Zadvornykh <s.zadvornykh@gmail.com>
2016-05-16 19:01:17 -0400

    trying vue.js setup

--- source/js/main.js
+++ source/js/main.js
@@ -3,13 +3,55 @@
 //=require three/*.js
 //=require objects/*.js
 //=require controllers/*.js
-//=require views/*.js
+//=require scenes/*.js
 //=require utils.js
 
 (function() {
-  var loader = new THREELoader(function() {
+  function load() {
+    var loader = new THREELoader(function() {
+      initVue();
+    });
 
-  });
+    loader.loadFont('main_font', 'res/fonts/droid_sans_bold.typeface.js');
+  }
 
-  loader.loadFont('main_font', 'res/fonts/droid_sans_bold.typeface.js');
+  function initVue() {
+    Vue.use(VueRouter);
+
+    var Intro = Vue.extend({});
+    var Default = Vue.extend({});
+
+    var scenes = [
+      {name:'00', component:Intro},
+      {name:'01'},
+      {name:'02'}
+    ];
+
+    //var App = Vue.extend({
+    //  data: function() {
+    //    return {scenes: [
+    //      {name:'00'},
+    //      {name:'01'},
+    //      {name:'02'}
+    //    ]};
+    //  }
+    //});
+    var router = new VueRouter();
+
+    scenes.forEach(function(scene) {
+      router.on('/' + scene.name, {
+        component: scene.component || Default
+      });
+    });
+
+    router.start({
+      data: function() {
+        return {
+          scenes:scenes
+        }
+      }
+    }, '#app')
+  }
+
+  load();
 })();
